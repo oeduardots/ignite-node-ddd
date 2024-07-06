0968dce9982f0308cafdd5108d02e025cb14aee2
Eduardo Tavares <edutav.dev@gmail.com>
2024-07-06 12:46:51 -0300

    path aliases e vitest globals

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
 import type { AnswersRepository } from '../repositories/answers-repository'
 import type { Answer } from '../entities/answer'
